4a26414058a6aebf0a0d124ba3f1bec74351b20b
Jonathan Giardino <j.giardino@me.com>
2020-08-18 12:52:28 +0200

    Create video component and controls

--- src/components/video.js
+++ src/components/video.js
@@ -1,50 +1,111 @@
-import React from "react"
+import React, { useState, useRef } from "react"
 import styled from "styled-components"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+// COMPONENTS
+import { BoxShadowLeft, BoxShadowRight } from "./boxShadows"
 
 const VideoWrapper = styled.div`
   position: relative;
 `
 
 const VideoContainer = styled.div`
-  position: absolute;
-  left: 8%;
-  right: 0;
+  position: relative;
+  margin-left: 8%;
   z-index: 3;
+  width: 65%;
 `
-const VideoSource = styled.video`
+const VideoSource = styled.figure`
   margin-top: -3rem;
+  font-size: 0;
 `
 
-const BoxShadowLeft = styled.div`
+const ControlButton = styled.button`
   position: absolute;
-  top: 5rem;
-  left: 0;
-  width: 71rem;
-  height: 16rem;
-  background-color: lightgrey;
-  z-index: 0;
-  opacity: 0.2;
+  z-index: 10;
+  bottom: 50%;
+  left: 50%;
+  margin-left: -35px;
+  margin-bottom: -35px;
+  background-color: transparent;
+  border: none;
+  transition: opacity 0.3s ease-in-out;
 `
-const BoxShadowRight = styled.div`
+
+const ResumeLabel = styled.h4`
+  color: white;
+  font-size: 1.2rem;
+  font-family: "Hero IC Condensed Medium";
+  letter-spacing: 0.25rem;
+  text-transform: uppercase;
   position: absolute;
-  top: -5rem;
-  right: 10.25rem;
-  width: 40rem;
-  height: 10rem;
-  background-color: lightgrey;
-  z-index: 0;
-  opacity: 0.2;
+  z-index: 10;
+  padding-left: 0.1rem;
+  transition: opacity 0.3s ease-in-out;
 `
 
 const Video = ({ testVideo }) => {
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const video = useRef(null)
+
+  const playPauseMedia = () => {
+    if (video.paused || video.ended) {
+      console.log(video)
+      video.play()
+      setIsPlaying(!isPlaying)
+    } else {
+      setIsPlaying(!isPlaying)
+      // video.pause()
+    }
+  }
+
+  const data = useStaticQuery(graphql`
+    query {
+      play: file(relativePath: { eq: "play.png" }) {
+        childImageSharp {
+          fixed(width: 70, height: 70) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      pause: file(relativePath: { eq: "pause.png" }) {
+        childImageSharp {
+          fixed(width: 70, height: 70) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
   return (
     <VideoWrapper>
       <BoxShadowLeft />
       <BoxShadowRight />
       <VideoContainer>
-        <VideoSource autoPlay muted loop controls height="450">
-          <source src={testVideo} type="video/mp4" />
-          Your browser does not support HTML5 video.
+        <VideoSource>
+          <ControlButton
+            style={{ opacity: isPlaying ? "0" : "1" }}
+            onClick={() => setIsPlaying(playPauseMedia)}
+          >
+            <Img fixed={data.play.childImageSharp.fixed} />
+            <ResumeLabel>Resume</ResumeLabel>
+          </ControlButton>
+          <ControlButton
+            style={{ opacity: isPlaying ? "1" : "0" }}
+            onClick={() => setIsPlaying(playPauseMedia)}
+          >
+            <Img fixed={data.pause.childImageSharp.fixed} />
+          </ControlButton>
+          <video
+            ref={video}
+            src={testVideo}
+            type="video/mp4"
+            muted
+            loop
+            width="100%"
+          ></video>
         </VideoSource>
       </VideoContainer>
     </VideoWrapper>
